8d4b9f9ea8d2c05986a966fabc782489d758582f
micorplatam-cell <micorp.latam@gmail.com>
2025-12-11 08:20:58 +0000

    fix: Agregar configuración de ESLint para CI/CD

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+const expo = require('eslint-config-expo');
+const tsParser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    ignores: [
+      'node_modules/**',
+      '.expo/**',
+      'dist/**',
+      'build/**',
+      'coverage/**',
+      '*.config.js',
+      '*.config.ts',
+    ],
+  },
+  ...expo,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+  },
+];
